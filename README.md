94a735b82f8df0568a7c8387717249c07228103d
model313 <dontgoap@gmail.com>
2024-07-25 15:27:18 +0900

    테스트

---
+++ README.md
@@ -0,0 +1,2 @@
+# git_javatest
+# git_javatest
---
+++ shopping/src/shopping/test.java
@@ -0,0 +1,9 @@
+package shopping;
+
+public class test {
+
+	public static void main(String[] args) {
+		String mid = "ȫ�浿";
+		System.out.println(mid);
+	}
+}
